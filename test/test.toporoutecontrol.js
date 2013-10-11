d74da5a6d33899abe4fb7b8bea7e77de79331c12
Mathieu Leplatre <mathieu.leplatre@makina-corpus.com>
2013-10-11 11:15:13 +0200

    Test activable control

--- test/test.toporoutecontrol.js
+++ test/test.toporoutecontrol.js
@@ -1,9 +1,47 @@
 var assert = chai.assert;
 
-describe('L.Handler.TopoRoute', function() {
+describe('L.Mixin.ActivableControl', function() {
 
-    describe('Activation', function() {
+    var Control = L.Control.Zoom.extend({
+        includes: L.Mixin.ActivableControl,
+    });
 
+    describe('Activable state', function() {
+
+        it('should be deactivated by default', function(done) {
+            var control = new Control();
+            assert.isFalse(!!control._activable);
+            done();
+        });
+
+        it('should become activable', function(done) {
+            var control = new Control();
+            control.activable(true);
+            assert.isTrue(control._activable);
+            control.activable(false);
+            assert.isFalse(control._activable);
+            done();
+        });
+
+        it('should not enable handler if not activable', function(done) {
+            var control = new Control();
+            control.handler = new L.Handler();
+            assert.isFalse(control.handler.enabled());
+            control.activate();
+            assert.isFalse(control.handler.enabled());
+            done();
+        });
+
+        it('should enable handler if activable', function(done) {
+            var map = L.map('map');
+            var control = (new Control()).addTo(map);
+            control.handler = new L.Map.Drag(map);
+            control.activable(true);
+            assert.isFalse(control.handler.enabled());
+            control.activate();
+            assert.isTrue(control.handler.enabled());
+            done();
+        });
     });
 
 });
